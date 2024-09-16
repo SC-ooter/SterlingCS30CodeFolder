553a89c66de6b50717368a1e6d22a6581bbe8607
SC-ooter <sterling.cossar@rcsd.ca>
2024-09-16 08:53:35 -0600

    Started Project

---
+++ 1-Lessons and Demos/Lesson4demo/src/App.java
@@ -0,0 +1,12 @@
+// Project: Lesson 4 Demmo
+// Name: Sterling Cossar
+// Date: 13 Sept 2024
+// Description: Going over comments and special characters
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\tHello, World!");
+        System.out.println("How was your day?");
+        System.out.println("Its cloudy with a cha\nce of meatballs today!");
+    }
+}
---
+++ A4 ASCII Art/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("\t  _   _  \n\t( . | . )");
+        System.out.println("                                               ");
+        System.out.println("###############################################");
+        System.out.println("###############################################");
+        System.out.println("###############################################");
+        System.out.println("###############################################");
+        System.out.println("###############################################");
+        System.out.println("###############################################");
+        System.out.println("###############################################");
+        System.out.println("###############################################");
+        System.out.println("###############################################");
+    }
+}
