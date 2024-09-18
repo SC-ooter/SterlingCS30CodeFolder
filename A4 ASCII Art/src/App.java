f0654abde06a511e002390fe69aafef9fd4c6f06
SC-ooter <sterling.cossar@rcsd.ca>
2024-09-18 08:52:59 -0600

    Tuesday Demos

--- A4 ASCII Art/src/App.java
+++ A4 ASCII Art/src/App.java
@@ -1,6 +1,14 @@
+// Yoohoo
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("\n\n                    _________\n                   |||      \\\\\n  _________________|||_______\\\\______________");
-        System.out.println(" |_|               || -       |             <|#########\n |      _______    ||         |   _______    |\n(______//     \\\\___||_________|__//     \\\\____)  \n________\\_____/___________________\\_____/_________________\n");
+        System.out.println("\n\n                        _________");
+        System.out.println("                       |||      \\\\");
+        System.out.println("     __________________|||_____[}\\\\____________");
+        System.out.println("    |__                || -     \\ |           ,|#########");
+        System.out.println("    |>|   _______      ||         |  _______   |");
+        System.out.println("   (_____//     \\\\_____||_________|_//     \\\\___)  ");
+        System.out.println("__________\\_____/____________________\\_____/_____________\n");
     }
 }
