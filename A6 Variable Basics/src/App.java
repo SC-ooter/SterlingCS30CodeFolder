886b123f7af64583c13c7919dca2e626846027c6
SC-ooter <sterling.cossar@rcsd.ca>
2024-09-19 09:49:14 -0600

    Thursday

---
+++ A6 Variable Basics/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+
+// Program name: (Assignment 6 Variable Basics)
+
+// Student name: (Sterling Cossar)
+
+// Date: (Thursday September 19th)
+
+// Program Description:
+
+    public static void main(String[] args) throws Exception {
+        double chicken = 0.75;
+        double total = (5.75/chicken);
+        System.out.println("If you have $4.75 you can buy " + (int) total + " Chicken wings");
+        System.out.println((total));
+
+    }
+}
