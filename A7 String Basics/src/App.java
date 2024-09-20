79e62d65998abced9833131acff0fd3314506651
SC-ooter <sterling.cossar@rcsd.ca>
2024-09-20 09:35:19 -0600

    Friday

---
+++ A7 String Basics/src/App.java
@@ -0,0 +1,25 @@
+public class App {
+// Program name: (The name you gave the program)
+
+// Student name: (Your name)
+
+// Date: (The date you completed the program)
+
+// Program Description:
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        String message1 = "I am very happy!";
+        String target1 = "very";
+
+        String message2 = "That was great - lol";
+        String target2 = "lol";
+    
+        System.out.println(message1);
+        System.out.println(message2);
+        
+        System.out.println(message1.length("very"));
+
+
+    }
+}
