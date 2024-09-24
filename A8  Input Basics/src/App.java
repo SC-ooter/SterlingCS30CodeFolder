919b70bc7eaaadf19eab7895e97e26417f65a91f
SC-ooter <sterling.cossar@rcsd.ca>
2024-09-24 09:48:43 -0600

    Tuesday

---
+++ A8  Input Basics/src/App.java
@@ -0,0 +1,39 @@
+// Program name: (The name you gave the program)
+
+// Student name: (Your name)
+
+// Date: (The date you completed the program)
+
+// Program Description:
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+
+
+
+      Scanner keyboard = new Scanner(System.in);
+
+      int intOne, intTwo; 
+
+      System.out.print("Enter an integer :: ");
+
+      intOne = keyboard.nextInt();
+
+ 
+
+      System.out.print("Enter an integer :: ");
+
+      intTwo = keyboard.nextInt();
+
+ 
+
+      System.out.println("integer one = " + intOne);
+
+      System.out.println("integer two = " + intTwo);
+
+   }
+
