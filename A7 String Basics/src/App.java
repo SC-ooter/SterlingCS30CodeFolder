e2677cc3e2545a9154c9122af0133993f26abddc
SC-ooter <sterling.cossar@rcsd.ca>
2024-09-24 09:40:10 -0600

    Finished A7 Strings Assignment

--- A7 String Basics/src/App.java
+++ A7 String Basics/src/App.java
@@ -1,27 +1,28 @@
 public class App {
 // Program name: (A7 String Basics)
 
-// Student name: (Your name)
+// Student name: (Sterling Cossar)
 
-// Date: (The date you completed the program)
+// Date: (September 24th 2024)
 
-// Program Description:
+// Program Description: 
     
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         String message1 = "I am very happy!";
         String target1 = "very";
 
         String message2 = "That was great - lol";
-        String target2 = "laugh out loud";
+        String target2 = "lol";
 
-        System.out.println(message1.indexOf(target1)+(message1));
-        
+        //Message 1
+        Integer message1pos1 = (message1.indexOf(target1));
+        Integer message1pos2 = (target1.length());
+        System.out.println(message1.substring(0,message1pos1 - 1)+ message1.substring(message1pos1 + message1pos2));
 
-
-    
-        System.out.println(message1.substring(indexOf(target1)) + message1.substring(9));
-        // System.out.println(message2.substring(0,17) + (target2));
+        //Message 2
+        Integer message2pos1 = (message2.length());
+        Integer message2pos2 = (target2.length());
+        System.out.println(message2.substring(0,message2pos1 - message2pos2) + ("laugh out loud"));
       
  }
 }
