a8c1ab25929be03ce8875c85278cbbb3946d1204
SC-ooter <sterling.cossar@rcsd.ca>
2024-09-27 08:57:04 -0600

    Finished input basics

--- A8  Input Basics/src/App.java
+++ A8  Input Basics/src/App.java
@@ -14,38 +14,24 @@ public static void main(String[] args) throws Exception {
   Scanner keyboard = new Scanner(System.in);
 
   int intOne, intTwo; 
-
   System.out.print("Enter an integer :: ");
-
   intOne = keyboard.nextInt();
-
   System.out.print("Enter another integer :: ");
-
   intTwo = keyboard.nextInt();
-
   System.out.println("integer one = " + intOne);
-
   System.out.println("integer two = " + intTwo);
-
   System.out.println("intOne + intTwo = " + (intOne + intTwo));
+
 
   // Doubles
 
   double doubleOne, doubleTwo; 
-
   System.out.print("Enter a double :: ");
-
   doubleOne = keyboard.nextDouble();
-
   System.out.print("Enter another double :: ");
-
   doubleTwo = keyboard.nextDouble();
-
   System.out.println("Double one = " + (double) doubleOne);
-
   System.out.println("Double two = " + (double) doubleTwo);
-
   System.out.println("doubleOne + doubleTwo = " + (double) (doubleOne + doubleTwo));
-
  }
 }
