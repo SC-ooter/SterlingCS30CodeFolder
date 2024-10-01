332a283c088e384fc2840d08d7ca34a9e4cd88a6
SC-ooter <sterling.cossar@rcsd.ca>
2024-10-01 09:49:03 -0600

    Tuesday

--- A8B Classes/src/App.java
+++ A8B Classes/src/App.java
@@ -1,5 +1,18 @@
+// Program name: (A8b Classes)
+
+// Student name: (Sterling Cossar)
+
+// Date: (Tuesday October 1st 2024)
+
+// Program Description: This progam uses the class dog to print the bark and woof through calling the class funtion
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Dog dog = new Dog();
+        System.out.println(dog);
+        dog.speak();
+        dog.Sayplease();
+        dog.speak();
+    
     }
 }
---
+++ A8B Classes/src/Dog.java
@@ -0,0 +1,9 @@
+public class Dog {
+    public void speak(){
+        System.out.println("Woof");
+
+    }
+    public void Sayplease(){
+        System.out.println("bark");
+}
+}
