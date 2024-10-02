2de315fc230f5b38c19a98dc56407ebcd9c813af
SC-ooter <sterling.cossar@rcsd.ca>
2024-10-02 09:31:08 -0600

    Demo

---
+++ 1-Lessons and Demos/Conditionals Demo/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x = 2;
+        int y = 3;
+
+        if (!(x > 0) || !(y < 2)) {
+            System.out.println("Wear a coat");
+            System.out.println("Wear gloves");
+        }
+
+        System.out.println("bye");
+    }
+}
