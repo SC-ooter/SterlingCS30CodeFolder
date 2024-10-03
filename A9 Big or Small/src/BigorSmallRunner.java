5783651faf8b94cbeba9e3433ccb488aa9920687
SC-ooter <sterling.cossar@rcsd.ca>
2024-10-03 09:46:58 -0600

    Thursday started A9

---
+++ A9 Big or Small/src/BigorSmallRunner.java
@@ -0,0 +1,20 @@
+// Program name: (A( Big or Small))
+
+// Student name: (Sterling Cossar)
+
+// Date: ()
+
+// Program Description:
+import java.util.Scanner;
+
+public class BigorSmallRunner {
+
+    public static void main(String[] args) throws Exception {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Hello, World!");
+        System.out.println("Allo Mate");
+        int intOne, intTwo;
+        System.out.print("Enter an integer :: ");
+        intOne = keyboard.nextInt();
+    }
+}
