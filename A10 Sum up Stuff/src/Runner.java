af94daf64ec31024b819f45ad08b7a1b8235ebbc
SC-ooter <sterling.cossar@rcsd.ca>
2024-10-09 09:49:00 -0600

    Finished Sum up stuff

---
+++ A10 Sum up Stuff/src/Runner.java
@@ -0,0 +1,25 @@
+// Program name: (A10 Sum up Stuff)
+
+// Student name: (Sterling Cossar)
+
+// Date: (Wednesday October 9th 2024)
+
+// Program Description:
+
+
+
+
+public class Runner{
+    public static void main(String[] args){
+        Sum test = new Sum();				
+		test.setNums(2,6);
+		test.sum();
+		test.printsum();
+		test.setNums(9,6);
+		test.sum();
+		test.printsum();
+        test.setNums(20,670);
+		test.sum();
+		test.printsum();	
+	}
+}
---
+++ A10 Sum up Stuff/src/Sum.java
@@ -0,0 +1,13 @@
+public class Sum{
+	private double num1, num2, sum;
+	public void setNums(double a, double b){
+		num1 = a;
+		num2 = b;
+	}
+	public void sum(){
+		sum = num1+num2;
+	}
+	public void printsum(){
+		System.out.println(sum);
+	}
+}
