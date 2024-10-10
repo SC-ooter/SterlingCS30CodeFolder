09290e79fec9a65d6789db55b0dc18028ec2654b
SC-ooter <sterling.cossar@rcsd.ca>
2024-10-10 09:45:34 -0600

    Thursday

---
+++ A12 FtoC Convert/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+            double a = (3/4);
+            int b = (98-32);
+        System.out.println(a);
+    }
+}
