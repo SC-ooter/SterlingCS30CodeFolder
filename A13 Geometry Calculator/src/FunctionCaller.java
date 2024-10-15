fa68a12668c1d6795febc133592937e533c0c2fe
SC-ooter <sterling.cossar@rcsd.ca>
2024-10-15 09:49:23 -0600

    Started Calc

---
+++ A13 Geometry Calculator/src/FunctionCaller.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+Scanner input = new Scanner(System.in);
+
+public class FunctionCaller {
+    public static Decide(){
+        System.out.println("Cheese");
+        input.nextInt();
+    }
+    
+
+}
