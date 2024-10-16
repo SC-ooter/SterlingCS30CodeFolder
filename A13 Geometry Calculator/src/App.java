0dfc5bfcc23e5dc26877b101c8425865704460de
SC-ooter <sterling.cossar@rcsd.ca>
2024-10-16 09:49:31 -0600

    Wednesday

--- A13 Geometry Calculator/src/App.java
+++ A13 Geometry Calculator/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        FunctionCaller runner= new FunctionCaller();
+        FunctionCaller runner = new FunctionCaller();
         runner.Decide();
     }
 }
--- A13 Geometry Calculator/src/FunctionCaller.java
+++ A13 Geometry Calculator/src/FunctionCaller.java
@@ -1,10 +1,37 @@
 import java.util.Scanner;
-Scanner input = new Scanner(System.in);
 
 public class FunctionCaller {
-    public static Decide(){
-        System.out.println("Cheese");
-        input.nextInt();
+    public void Decide(){
+        Scanner input = new Scanner(System.in);
+        int Choice1;
+        System.out.print("Make your choice! Input 1 if you want to calculate rectangles, 2 if you want to calculate squares, and 3 if you want to calculate circles : ");
+        Choice1 = input.nextInt();
+
+        if (Choice1==1){
+            Scanner length = new Scanner(System.in);
+            double Length1, Width1;
+            System.out.print("Rectangles :: Enter a length : ");
+            Length1 = length.nextInt();
+
+            Scanner width = new Scanner(System.in);
+            System.out.print("Enter a width : ");
+            Width1 = width.nextInt();
+
+            System.out.println("The perimeter of your rectangle is " + ((2*Length1)+(2*Width1)) + " units"); 
+        }
+        if (Choice1==2){
+            Scanner side = new Scanner(System.in);
+            double Side;
+            System.out.println("square");
+            Side = side.nextInt();
+            System.out.println(6  *(Math.pow(Side,2)));
+        }
+
+        else if (Choice1 != 1){
+            System.out.println("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+
+        }
+
     }
     
 
