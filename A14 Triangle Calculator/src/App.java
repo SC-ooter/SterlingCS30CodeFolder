ced6d3f72b848b7764ba59b90e8f80af332ab275
SC-ooter <sterling.cossar@rcsd.ca>
2024-10-23 09:49:42 -0600

    Wed

---
+++ A14 Triangle Calculator/src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Functions runner = new Functions();
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input a value for a side of a triangle : ");
+        int SideA = input.nextInt();
+
+        System.out.print("bobv");
+        int SideB = input.nextInt();
+
+        System.out.print("ui");
+        int SideC = input.nextInt();
+
+
+    }
+
+}
---
+++ A14 Triangle Calculator/src/Functions.java
@@ -0,0 +1,24 @@
+// Program name: (The name you gave the program)
+
+// Student name: (Your name)
+
+// Date: (The date you completed the program)
+
+// Program Description:
+import java.util.Scanner;
+public class Functions {
+    private int SideA;
+    private int SideB;
+    private int SideC;
+    
+    public void Sides(int a, int b, int c){
+        SideA = a;
+        SideB = b;
+        SideC = c;
+
+    }
+    public double Triperm(){
+        return SideA + SideB + SideC;
+
+    }
+}
