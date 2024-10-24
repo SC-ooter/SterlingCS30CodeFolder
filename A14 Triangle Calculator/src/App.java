6d6ca9029a68c55b84b48d40b789e4b231990e12
SC-ooter <sterling.cossar@rcsd.ca>
2024-10-24 09:25:51 -0600

    Finished Triangle calculator

--- A14 Triangle Calculator/src/App.java
+++ A14 Triangle Calculator/src/App.java
@@ -3,17 +3,26 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        Functions runner = new Functions();
+        
         Scanner input = new Scanner(System.in);
 
         System.out.print("Input a value for a side of a triangle : ");
-        int SideA = input.nextInt();
+        double SideA = input.nextDouble();
 
-        System.out.print("bobv");
-        int SideB = input.nextInt();
+        System.out.print("Input the second side length : ");
+        double SideB = input.nextDouble();
 
-        System.out.print("ui");
-        int SideC = input.nextInt();
+        System.out.print("Input the third side length : ");
+        double SideC = input.nextDouble();
+
+        Functions runner = new Functions(SideA, SideB, SideC);
+        double perimeter = runner.Triperm();
+        System.out.println("The perimeter of the triangle is "+ (perimeter));
+
+        double area = runner.TriArea();
+        System.out.println("The area of the triangle is " + (area));
+
+
 
 
     }
--- A14 Triangle Calculator/src/Functions.java
+++ A14 Triangle Calculator/src/Functions.java
@@ -1,17 +1,17 @@
-// Program name: (The name you gave the program)
+// Program name: (A14 Triangle Calculator)
 
-// Student name: (Your name)
+// Student name: (Sterling Cossar)
 
-// Date: (The date you completed the program)
+// Date: (Thursday October 24th 2024)
 
-// Program Description:
-import java.util.Scanner;
+// Program Description: Uses the class Functions to name all of my variables and it creates the functions to do the calculations with the variables. My app class takes the user input and calls the functions to return the perimeter and the area.
+
 public class Functions {
-    private int SideA;
-    private int SideB;
-    private int SideC;
-    
-    public void Sides(int a, int b, int c){
+    private double SideA;
+    private double SideB;
+    private double SideC;
+
+    public Functions(double a, double b, double c) {
         SideA = a;
         SideB = b;
         SideC = c;
@@ -19,6 +19,9 @@ public class Functions {
     }
     public double Triperm(){
         return SideA + SideB + SideC;
-
+    }
+    public double TriArea(){
+        double area = Triperm() / 2;
+        return Math.sqrt(area *(area - SideA)*(area - SideB)*(area - SideC));
     }
 }
