3d2d8ea7d8f7be21b2cb91220c52df8b6d42ed2c
SC-ooter <sterling.cossar@rcsd.ca>
2024-10-28 09:39:32 -0600

    Monday - Finished MPH

--- A17 Miles per Hour/src/App.java
+++ A17 Miles per Hour/src/App.java
@@ -8,12 +8,16 @@ public class App {
 
         System.out.print("Please enter the time elapsed in minutes : ");
         double TimeM = input.nextDouble();
-        MPH runner = new MPH(TimeH, TimeM, TimeM);
-        double Min = runner.Minutes();
-        System.out.print(Min);
 
+        
         System.out.print("Please enter the miles traveled : ");
         double Miles = input.nextDouble();
+
+
+        MPH runner = new MPH(TimeM, TimeH, Miles);
+        double Min = runner.Minutes();
+        System.out.println("If you were traveling for " + TimeH + " hours and " + TimeM + " minutes you would have been going " + Min + " miles per hour.");
+
 
 
 
--- A17 Miles per Hour/src/MPH.java
+++ A17 Miles per Hour/src/MPH.java
@@ -1,3 +1,11 @@
+// Program name: (A17 Miles Per Hour)
+
+// Student name: (Sterling Cossar)
+
+// Date: (October 28th 2024)
+
+// Program Description: Assigns all the vaiables here then prints out what the user needs to do. When the user input the values the functions will put the values in the equations and out put how fast the person was going.
+
 public class MPH{
     private double TimeH;
     private double TimeM;
@@ -10,13 +18,9 @@ public class MPH{
 
     }
     public double Minutes(){
-        double total = (double) TimeH + (TimeM / 60);
-        if (total == 0);{
-            return 0;
-        }
+        double total = (double)TimeH + (TimeM / 60);
+        return Miles / total;
     }
-    
 
-
-    }
+}
 
