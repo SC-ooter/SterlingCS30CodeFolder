066a0cc08e28c93f0df2cce956786efd3d13d4ef
SC-ooter <sterling.cossar@rcsd.ca>
2024-11-01 09:44:33 -0600

    Friday

--- A23 Cool numbers/src/App.java
+++ A23 Cool numbers/src/App.java
@@ -5,10 +5,7 @@ public class App {
         System.out.print("Please enter a value to check for cool numbers : ");
         int numbers = input.nextInt();
 
-        CoolNums coolnums = new CoolNums();
-        System.out.println(numbers);
-        while ((numbers % 3 == 1) && (numbers % 4 == 1) && (numbers % 5 == 1) && (numbers % 6 == 1)){
-            System.out.print("weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+        int countcoolnumbers = CoolNums.countcoolnumbers(numbers);
+        System.out.println(countcoolnumbers);
         }
     }
-}
--- A23 Cool numbers/src/CoolNums.java
+++ A23 Cool numbers/src/CoolNums.java
@@ -1,9 +1,15 @@
 public class CoolNums {
     private int Coolnums;
-
-    public void checkcoolnumbers(){
-
+    public static boolean checkcoolnumbers(int numbers){
+        return ((numbers % 3 == 1) && (numbers % 4 == 1) && (numbers % 5 == 1) && (numbers % 6 == 1));
     }
-
-
+    public static int countcoolnumbers(int numbers) {
+        int count = 0;
+        for (int x = 6; x < numbers + 1; x++){
+            if (checkcoolnumbers(x)){
+            count++;
+        }
+    }
+    return count;
+    }
 }
