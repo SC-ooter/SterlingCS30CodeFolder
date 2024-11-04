09484cab58e765d927a2f2c58660f8da1ef497d2
SC-ooter <sterling.cossar@rcsd.ca>
2024-11-04 08:58:46 -0600

    Submitted Cool numbers

--- A23 Cool numbers/src/App.java
+++ A23 Cool numbers/src/App.java
@@ -1,3 +1,12 @@
+// Program name: (A23 Cool numbers)
+
+// Student name: (Sterling Cossar)
+
+// Date: (November 1st 2024)
+
+// Program Description: This program uses the user input and runs the input through different modulus calculations to see what numbers from the users input to six have a remainder of 1 when you divide it by 6,5,4,3.
+
+
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
