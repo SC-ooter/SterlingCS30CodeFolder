a7aacce3756682356566cb77dcc97be0139c156d
SC-ooter <sterling.cossar@rcsd.ca>
2024-11-04 09:48:45 -0600

    Tried to start Loop stats

---
+++ A24 Loop stats/src/LoopStats.java
@@ -0,0 +1,67 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name
+
+import java.util.Scanner;
+
+public class LoopStats
+{
+	private int start, stop;
+
+	public LoopStats(int x)
+	{
+
+	}
+
+
+	// public LoopStats(int beg, int end)
+	// {
+	// }
+
+	public void setNums(int beg, int end)
+	{
+
+
+
+	}
+
+	public int getEvenCount()
+	{
+		int evenCount=0;
+
+
+
+
+		return evenCount;
+	}
+
+	public int getOddCount()
+	{
+		int oddCount=0;
+
+
+
+
+
+		return oddCount;
+	}
+
+	public int getTotal()
+	{
+		int total=0;
+
+
+
+
+		return total;
+	}
+	
+	public String toString()
+	{
+		return start + " " + stop;
+	}
+
+
+
+}
+
---
+++ A24 Loop stats/src/LoopStatsRunner.java
@@ -0,0 +1,15 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name
+import java.util.Scanner;
+
+public class LoopStatsRunner
+{
+	public static void main ( String[] args )
+	{
+		Scanner Keyboard = new Scanner(System.in);
+		int numbers = Keyboard.nextInt();
+
+		//add test cases		
+	}
+}
