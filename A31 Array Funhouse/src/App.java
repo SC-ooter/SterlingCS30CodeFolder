38abef13fb9d5761254da4c8f126ffb6c494dbdd
SC-ooter <sterling.cossar@rcsd.ca>
2024-11-28 09:20:44 -0600

    Submitted Funhouse

--- A31 Array Funhouse/src/App.java
+++ A31 Array Funhouse/src/App.java
@@ -1,18 +1,43 @@
+// Program name: (A31 ARRAY FUNHOUSE)
+
+// Student name: (STERLING COSSAR)
+
+// Date: (NOVEMBER 28 2024)
+
+// Program Description: USES DIFFERENT FUNCTIONS AND FOR LOOPS TO MANIPULATE THE SET OF NUMBERS. PRINTS OUT ALL THE NUMBERS AND USES PARAMETERS TO COUNT CERTAIN NUMBERS AND REMOVE OTHERS
+
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         int[] arr = {7, 4, 10, 0, 1, 7, 6, 5, 3, 2, 9, 7};
-        System.out.println(count(arr, 0));
-        System.out.println(getSum(arr, 0, 0));
-        System.out.println(removeVal(arr, 0));
-        
-    }
-        // int[] arr2 = {7, 4, 2, 7, 3, 4, 6, 7, 8, 9, 7, 0, 10, 7, 0, 1, 7, 6, 5, 7, 3, 2, 7, 9, 9, 8,7};
 
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Sum of 3-6: " + getSum(arr, 3, 6));
+        System.out.println("Sum of 2-9: " + getSum(arr, 2,9));
+        System.out.println( ("number of 4s: ") + count(arr, 4));
+        System.out.println( ("number of 7s: ") + count(arr, 7));
+        System.out.println( ("number of 9s: ") + count(arr, 9));
+        arr = removeVal(arr, 7);
+        System.out.println("New array without 7: " + Arrays.toString(arr));
+
+        int[] arr2 = {7, 4, 2, 7, 3, 4, 6, 7, 8, 9, 7, 0, 10, 7, 0, 1, 7, 6, 5, 7, 3, 2, 7, 9, 9, 8,7};
+
+        System.out.println("\n\n"+ Arrays.toString(arr2));
+        System.out.println("Sum of 3-6: " + getSum(arr2, 3, 6));
+        System.out.println("Sum of 2-9: " + getSum(arr2, 2,9));
+        System.out.println( ("number of 4s: ") + count(arr2, 4));
+        System.out.println( ("number of 7s: ") + count(arr2, 7));
+        System.out.println( ("number of 9s: ") + count(arr2, 9));
+        arr2 = removeVal(arr2, 7);
+        System.out.println("New array without 7: " + Arrays.toString(arr2));
+    }
+    
     public static int getSum(int[] arr, int start, int end) {
         int sum = 0;
         for (int i = start; i <= end; i++) {
-            sum = sum += arr[i];
+            sum = sum + arr[i];
         }
         return sum;
     }
@@ -25,56 +50,17 @@ public class App {
         }
         return count;
     }
-    public static int removeVal(int[] arr, int val){
+    public static int[] removeVal(int[] arr, int val){
         int size = arr.length - count(arr,val);
-        int x = 0;
+        int[] x = new int[size];
 
+        int u = 0;
         for (int i= 0; i<arr.length; i++){
             if (arr[i] != val){
-                x = arr[i];
-                x++;
+                x[u] = arr[i];
+                u++;
             }
         }
         return x ;
     }
 }
-
-
-//     public static int countOccurrences(int[] arr, int number) {
-//         int count = 0;
-//         for (int num : arr) {
-//             if (num == number) {
-//                 count++;
-//             }
-//         }
-//         return count;
-//     }
-//     public static int[] removeValue(int[] arr, int value) {
-//         List<Integer> resultList = new ArrayList<>();
-//         for (int num : arr) {
-//             if (num != value) {
-//                 resultList.add(num);
-//             }
-//         }
-//         int[] resultArr = new int[resultList.size()];
-//         for (int i = 0; i < resultList.size(); i++) {
-//             resultArr[i] = resultList.get(i);
-//         }
-//         return resultArr;
-//     }
-//     public static void main(String[] args) {
-//         int[] arr = {7, 4, 10, 0, 1, 7, 6, 5, 3, 2, 9, 7};
-
-//         System.out.println("Sum of section (3 to 6): " + sumSection(arr, 3, 6));
-
-//         System.out.println("Occurrences of 7: " + countOccurrences(arr, 7)); 
-
-
-//         int[] resultArr = removeValue(arr, 7);
-//         System.out.print("Array after removing all 7's: ");
-//         for (int num : resultArr) {
-//             System.out.print(num + " "); 
-//         }
-//     }
-// }
-// }
