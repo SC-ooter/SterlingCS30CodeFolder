6bbed6095e7d2529e51a19b349bb8dea76bfcaa2
SC-ooter <sterling.cossar@rcsd.ca>
2024-12-12 09:49:44 -0600

    Thursday

--- A37 Array List Raffle tickets/src/App.java
+++ A37 Array List Raffle tickets/src/App.java
@@ -1,7 +1,15 @@
-import java.util.List;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        String[] names = new String[]{"Mannion", "Fox", "Wolf", "Sutter", "Bacheleu", "Zeck", "Robertson", "Kosloski", "Cook", "Maier"};
+        List<String> Arraylists = new ArrayList<String>();
+        System.out.println("Names :" + Arrays.toString(names));
+        System.out.println(names.length);
+        names.remove(5);
+        System.out.println(names);
+        }
+
+
     }
-}
