ca521a0777d98aab096cd9bc1496524affbc6a33
SC-ooter <sterling.cossar@rcsd.ca>
2024-12-17 09:47:40 -0600

    tuesday

--- 1-Lessons and Demos/bubble sort demo/src/App.java
+++ 1-Lessons and Demos/bubble sort demo/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        int[] numList = {5,15,3,8,9,1,20,7};
+        int[] numList = {5, 15, 3, 8, 9, 1, 20, 7};
         //printing original list
         for (int i = 0; i<numList.length; i++){
             System.out.println(numList[i]);
@@ -15,10 +15,36 @@ public class App {
         }
     }
 
+    public static int[] selsort(int[] nums) {
+        int n = nums.length;
+// one by one every element of the array
+        for (int i = 0; i < n; i++) {
+
+            int minIDX = i;
+
+            for (int j = 0; j < n; j++){
+                if (nums[j] < nums[minIDX]) {
+                    minIDX = j;
+                }
+                
+            }
+        }
+
+            return nums;
+        
+    }
+
     public static int[] sort(int[] nums) {
+        int n = nums.length;
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
+            for (int j = 0; j < (n-i) - 1; j++) {
+                System.out.println("J = " + nums[j] + " ; J+1" + nums[j+1]);
                 if (nums[j] > nums[j+1]){
+                    System.out.println("SWAP");
+                    //swap the elements
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
 
                 }
             }
